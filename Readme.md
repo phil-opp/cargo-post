cfacf2cdb2b34c4b658ddcea7f7a36949a7cdb85
Philipp Oppermann <dev@phil-opp.com>
2019-03-21 14:16:03 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,94 @@
+# cargo-post
+
+<img style="float:right" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Aiga_mail_inverted.svg/114px-Aiga_mail_inverted.svg.png">
+
+A `cargo` wrapper that executes a post build script after a successful build.
+
+## Installation
+
+```
+cargo install cargo-post
+```
+
+## Usage
+
+Execute `cargo CMD [ARGS]` and run `post_build.rs` afterwards:
+
+```
+cargo post CMD [ARGS]
+```
+
+The `post_build.rs` is only run if `CMD` is a build command like `build` or [`xbuild`](http://github.com/rust-osdev/cargo-xbuild/).
+
+
+
+### Examples:
+
+Build the crate and run `post_build.rs` afterwards:
+
+```
+cargo post build
+```
+
+Build the crate in release mode and run `post_build.rs` afterwards:
+
+```
+cargo post build --release
+```
+
+Builds the crate using [cargo-xbuild](http://github.com/rust-osdev/cargo-xbuild/):
+
+```
+cargo post xbuild
+```
+
+Check the crate without executing post_build:
+
+```
+cargo post check
+```
+
+The build script is not executed because `cargo check` is not a build command. The same behavior occurs for `cargo post doc` or `cargo post update`.
+
+## Post-Build Script Format
+
+Post-build scripts are similar to cargo build scripts, but they get a different set of environment variables:
+
+- `CRATE_BUILD_COMMAND`: The full cargo command that was used for building without `post`
+    - Example: When the crate is compiled using `cargo post build --release`, the environment variable has the value `cargo build --release`.
+- `CRATE_MANIFEST_DIR`: The directory where the `Cargo.toml` of the crates lives.
+- `CRATE_PROFILE`: `debug` or `release`, depending on whether `--release` was passed in the build command.
+- `CRATE_TARGET`: The full content of what was passed as `--target` or the empty string if no `--target` was passed.
+- `CRATE_TARGET_TRIPLE`: The target triple of the passed `--target` or the empty string if no `--target` was passed.
+    - Example: With `cargo post xbuild --target /some/path/to/your/target-x86_64.json` this environment variable has the value `target-x86_64`.
+- `CRATE_TARGET_DIR`: The path to the `target` directory of your crate.
+- `CRATE_OUT_DIR`: The path to the directory where cargo puts the compiled binaries. This path is constructed by appending `CRATE_TARGET_TRIPLE` and `CRATE_PROFILE` to `CRATE_TARGET_DIR`.
+
+### Example
+
+```rust
+use std::env;
+
+fn main() {
+    println!("Hello from post build script!");
+    for (key, value) in env::vars() {
+        if key.starts_with("CRATE_") {
+            println!("{}: {}", key, value);
+        }
+    }
+}
+```
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+  http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
---
+++ src/main.rs
@@ -0,0 +1,191 @@
+use std::{
+    env, fs,
+    ops::Deref,
+    path::{Path, PathBuf},
+    process::{self, Command},
+};
+
+static HELP: &str = include_str!("help.txt");
+
+/// The required post_build script call
+enum BuildScriptCall {
+    /// No call the post build script is needed
+    ///
+    /// For example for `cargo check` or `cargo new`.
+    NoCall,
+    /// The build script needs to be run after executing the cargo command
+    ///
+    /// For example for `cargo build`.
+    AfterCommand,
+    // TODO: Special variants for e.g. `cargo run` where the post build script needs to be
+    // run in between (i.e. after the build, but before running it).
+}
+
+fn main() {
+    // check arguments
+    let mut args = env::args().peekable();
+    assert!(args.next().is_some(), "no executable name in args");
+    if args.next().as_ref().map(Deref::deref) != Some("post") {
+        panic!("cargo-post must be invoked as `cargo post`");
+    }
+    if args.peek().map(Deref::deref) == Some("--help") {
+        println!("{}", HELP);
+        return;
+    }
+    if args.peek().map(Deref::deref) == Some("--version") {
+        println!("cargo-post {}", env!("CARGO_PKG_VERSION"));
+        return;
+    }
+
+    let build_script_call = match args.peek().map(Deref::deref) {
+        Some(cmd) => match cmd {
+            "b" | "build" | "xbuild" => BuildScriptCall::AfterCommand,
+            "c" | "check" | "clean" | "doc" | "new" | "init" | "update" | "search"
+            | "uninstall" => BuildScriptCall::NoCall,
+            cmd if ["run", "test", "bench", "publish", "install"].contains(&cmd) => {
+                panic!("`cargo post {}` is not supported yet", cmd)
+            }
+            cmd => panic!("unknown cargo command `cargo {}`", cmd),
+        },
+        None => BuildScriptCall::NoCall,
+    };
+
+    // run cargo
+    let mut cmd = Command::new("cargo");
+    cmd.args(args);
+    let exit_status = match cmd.status() {
+        Ok(status) => status,
+        Err(err) => panic!("failed to execute command `{:?}`: {:?}", cmd, err),
+    };
+    if !exit_status.success() {
+        process::exit(exit_status.code().unwrap_or(1));
+    }
+
+    match build_script_call {
+        BuildScriptCall::NoCall => return,
+        BuildScriptCall::AfterCommand => {
+            if let Some(exit_status) = run_post_build_script() {
+                if !exit_status.success() {
+                    process::exit(exit_status.code().unwrap_or(1));
+                }
+            }
+        }
+    };
+}
+
+fn run_post_build_script() -> Option<process::ExitStatus> {
+    let mut cmd = cargo_metadata::MetadataCommand::new();
+    cmd.no_deps();
+    let manifest_path = {
+        let mut args = env::args().skip_while(|val| !val.starts_with("--manifest-path"));
+        match args.next() {
+            Some(ref p) if p == "--manifest-path" => Some(args.next().unwrap()),
+            Some(p) => Some(p.trim_start_matches("--manifest-path=").to_owned()),
+            None => None,
+        }
+    };
+    if let Some(ref manifest_path) = manifest_path {
+        cmd.manifest_path(manifest_path);
+    }
+    let metadata = cmd.exec().unwrap();
+
+    let package = {
+        let mut args =
+            env::args().skip_while(|val| !val.starts_with("--package") | !val.starts_with("-p"));
+        let package_name = match args.next() {
+            Some(ref p) if p == "--package" || p == "-p" => Some(args.next().unwrap()),
+            Some(p) => Some(p.trim_start_matches("--package=").to_owned()),
+            None => None,
+        };
+        let mut packages = metadata.packages.iter();
+        match package_name {
+            Some(name) => packages
+                .find(|p| p.name == name)
+                .expect("specified package not found"),
+            None => {
+                let package = packages.next().expect("workspace has no packages");
+                assert!(
+                    packages.next().is_none(),
+                    "Please specify a `--package` argument"
+                );
+                package
+            }
+        }
+    };
+
+    let manifest_path = manifest_path
+        .map(PathBuf::from)
+        .unwrap_or(package.manifest_path.clone());
+    let manifest_dir = manifest_path.parent().expect("failed to get crate folder");
+    let post_build_script_path = manifest_dir.join("post_build.rs");
+
+    if !post_build_script_path.exists() {
+        return None;
+    }
+    println!(
+        "Running Post Build Script at {}",
+        post_build_script_path.display()
+    );
+
+    // Create a dummy Cargo.toml for post build script
+    let build_script_manifest_dir = metadata
+        .target_directory
+        .clone()
+        .join("post_build_script_manifest");
+    fs::create_dir_all(&build_script_manifest_dir)
+        .expect("failed to create build script manifest dir");
+    let build_script_manifest_path = build_script_manifest_dir.join("Cargo.toml");
+    let build_script_manifest_content = format!(
+        include_str!("post_build_script_manifest.toml"),
+        file_name = post_build_script_path.display()
+    );
+    fs::write(&build_script_manifest_path, build_script_manifest_content)
+        .expect("Failed to write post build script manifest");
+
+    // gather arguments for post build script
+    let target_path = {
+        let mut args = env::args().skip_while(|val| !val.starts_with("--target"));
+        match args.next() {
+            Some(ref p) if p == "--target" => Some(args.next().expect("no target after --target")),
+            Some(p) => Some(p.trim_start_matches("--target=").to_owned()),
+            None => None,
+        }
+    };
+    let target_triple = {
+        let file_stem = target_path.as_ref().map(|t| Path::new(t).file_stem().expect("target has no file stem").to_owned());
+        file_stem.map(|s| s.into_string().expect("target not a valid string"))
+    };
+    let profile = if env::args().find(|arg| arg == "--release").is_some() {
+        "release"
+    } else {
+        "debug"
+    };
+    let mut out_dir = metadata.target_directory.clone();
+    if let Some(ref target_triple) = target_triple {
+        out_dir.push(target_triple);
+    }
+    out_dir.push(&profile);
+    let build_command = {
+        let mut cmd = String::from("cargo ");
+        let args: Vec<String> = env::args().skip(2).collect();
+        cmd.push_str(&args.join(" "));
+        cmd
+    };
+
+    // run post build script
+    let mut cmd = Command::new("cargo");
+    cmd.arg("run");
+    cmd.arg("--manifest-path");
+    cmd.arg(build_script_manifest_path.as_os_str());
+    cmd.env("CRATE_MANIFEST_DIR", manifest_dir.as_os_str());
+    cmd.env(
+        "CRATE_TARGET_DIR",
+        metadata.target_directory.as_os_str(),
+    );
+    cmd.env("CRATE_OUT_DIR", out_dir);
+    cmd.env("CRATE_TARGET", target_path.unwrap_or(String::new()));
+    cmd.env("CRATE_TARGET_TRIPLE", target_triple.unwrap_or(String::new()));
+    cmd.env("CRATE_PROFILE", profile);
+    cmd.env("CRATE_BUILD_COMMAND", build_command);
+    Some(cmd.status().expect("Failed to run post build script"))
+}
