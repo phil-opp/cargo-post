6454130ad7e0aa4225d3b54cb7a5b5c76cb13a0f
Philipp Oppermann <dev@phil-opp.com>
2020-07-19 17:05:11 +0200

    Create a simple test framework

---
+++ tests/simple/post_build.rs
@@ -0,0 +1,13 @@
+use std::{path::{Path, PathBuf}, env};
+
+fn main() {
+    assert_eq!(env::var("CRATE_BUILD_COMMAND").unwrap(), "cargo build --package simple");
+    assert_eq!(PathBuf::from(env::var("CRATE_MANIFEST_DIR").unwrap()), Path::new(".").canonicalize().unwrap());
+    assert_eq!(PathBuf::from(env::var("CRATE_MANIFEST_PATH").unwrap()), Path::new("Cargo.toml").canonicalize().unwrap());
+    assert_eq!(env::var("CRATE_PROFILE").unwrap(), "debug");
+    assert_eq!(env::var("CRATE_TARGET").unwrap(), "");
+    assert_eq!(env::var("CRATE_TARGET_TRIPLE").unwrap(), "");
+    assert_eq!(PathBuf::from(env::var("CRATE_TARGET_DIR").unwrap()), Path::new("../target").canonicalize().unwrap());
+    assert_eq!(PathBuf::from(env::var("CRATE_OUT_DIR").unwrap()), Path::new("../target").join("debug").canonicalize().unwrap());
+    println!("ok");
+}
