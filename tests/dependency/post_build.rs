3c87a74ae5288b7e4af14e5c865320b2a9511b99
NullP01nt <NullP01nt@users.noreply.github.com>
2025-09-30 13:52:21 +0200

    Fix unused import warning

--- tests/dependency/post_build.rs
+++ tests/dependency/post_build.rs
@@ -1,17 +1,32 @@
-use std::{path::{Path, PathBuf}, env};
+use std::{env, path::PathBuf};
 
 use example as _;
 
 fn main() {
     let current_dir = env::current_dir().unwrap();
     let current_parent = current_dir.parent().unwrap();
-    assert_eq!(env::var("CRATE_BUILD_COMMAND").unwrap(), "cargo build --package dependency");
-    assert_eq!(PathBuf::from(env::var("CRATE_MANIFEST_DIR").unwrap()), current_dir);
-    assert_eq!(PathBuf::from(env::var("CRATE_MANIFEST_PATH").unwrap()), current_dir.join("Cargo.toml"));
+    assert_eq!(
+        env::var("CRATE_BUILD_COMMAND").unwrap(),
+        "cargo build --package dependency"
+    );
+    assert_eq!(
+        PathBuf::from(env::var("CRATE_MANIFEST_DIR").unwrap()),
+        current_dir
+    );
+    assert_eq!(
+        PathBuf::from(env::var("CRATE_MANIFEST_PATH").unwrap()),
+        current_dir.join("Cargo.toml")
+    );
     assert_eq!(env::var("CRATE_PROFILE").unwrap(), "debug");
     assert_eq!(env::var("CRATE_TARGET").unwrap(), "");
     assert_eq!(env::var("CRATE_TARGET_TRIPLE").unwrap(), "");
-    assert_eq!(PathBuf::from(env::var("CRATE_TARGET_DIR").unwrap()), current_parent.join("target"));
-    assert_eq!(PathBuf::from(env::var("CRATE_OUT_DIR").unwrap()), current_parent.join("target").join("debug"));
+    assert_eq!(
+        PathBuf::from(env::var("CRATE_TARGET_DIR").unwrap()),
+        current_parent.join("target")
+    );
+    assert_eq!(
+        PathBuf::from(env::var("CRATE_OUT_DIR").unwrap()),
+        current_parent.join("target").join("debug")
+    );
     println!("ok");
 }
